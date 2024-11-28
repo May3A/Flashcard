1d18bf8f0b1251c2e9c1f9cc400bda84e60ee4c8
May3A <00001107825842sp@al.educacao.sp.gov.br>
2024-11-28 00:44:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Matematica',
+    'O que é a regra do cateto?',
+    'Em um triângulo retângulo a soma dos quadrados dos catetos é igual ao quadrado da hipotenusa'
+)
+
+criaCartao(
+    'Português',
+    'O que é cubismo na língua portuguesa?',
+    'Escola artística moderna que representa os objectos sob formas geométricas.'
+)
+
+criaCartao(
+    'Fisica',
+    'Qual é a formula mais dificil da fisica',
+    'Equação de Navier-Stokes'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da frança?',
+    'Paris'
+)
+
+criaCartao(
+    'historia',
+    'Quando começou a primeira guerra-mundial?',
+    '28 de julho de 1914'
+)
+
+criaCartao(
+    'Educação Fisica',
+    'Quais esporte utiliza raquetes?',
+    'Tênis, squash, tênis de mesa, badminton e frescobol'
+)
